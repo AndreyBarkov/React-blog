c545e5ad55ad3f389ca6142e7f11ee642b9abbfa
Andrey Barkov <a.barkov@avanade.com>
2018-03-26 09:57:57 -0400

    Starting setup redux. Added action types and action creators

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,6 @@
+export const NEW_POST = 'NEW_POST';
+export const EDIT_POST= 'EDIT_POST';
+export const EXISTING_POST = 'EXISTING_POST';
+export const ADD_POST = 'ADD_POST';
+export const REMOVE_POST = 'REMOVE_POST';
+export const UPDATE_POST ='UPDATE_POST';
---
+++ src/actions/actions.js
@@ -0,0 +1,35 @@
+import * as types from './actionTypes';
+
+export function newPost() {
+    return {
+        type: types.NEW_POST,
+    }
+}
+export function editPost() {
+    return {
+        type: types.EDIT_POST,
+    }
+}
+export function existingPost() {
+    return {
+        type: types.EXISTING_POST,
+    }
+}
+export function addPost(post) {
+    return {
+        type: types.ADD_POST,
+        post,
+    }
+}
+export function removePost(id) {
+    return {
+        type: types.REMOVE_POST,
+        id,
+    }
+}
+export function updatePost(post) {
+    return {
+        type: types.UPDATE_POST,
+        post
+    }
+}

ea36b4c1b38a9b95ff024cbbae4d29e91aff47f1
Andrey Barkov <a.barkov@avanade.com>
2018-03-27 11:17:14 -0400

    Added case for no posts to the list

--- src/components/List.js
+++ src/components/List.js
@@ -1,33 +1,42 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import React from "react";
+import PropTypes from "prop-types";
 
-
-const List = (props) => {
-     
-    const list =  props.posts.map((post, index) => 
-                                (<li 
-                                    key={post.id}
-                                    className = {post.id=== props.currentPost ? "selected" : ""}
-                                    onClick = { () => props.OnClick(post.id)} 
-                                > {post.title} </li>));
-    return(
-        <div className="post-list">All Posts
-            <ul>
-               {list}
-            </ul>
-
-        </div>
-    );
+const List = props => {
+  let list;
+  if(props.posts.length < 1){
+      console.log("no posts");
+      list = (<li>There are no posts yet</li>);
+  }
+  else{
+   list = props.posts.map((post, index) => (
+    <li
+      key={post.id}
+      className={post.id === props.currentPost ? "selected" : ""}
+      onClick={() => props.OnClick(post.id)}
+    >
+      {" "}
+      {post.title}{" "}
+    </li>
+  ));
 }
+  return (
+    <div className="post-list">
+      All Posts
+      <ul>{list}</ul>
+    </div>
+  );
+
+};
 List.propTypes = {
-    currentPost:PropTypes.number.isRequired,
-    OnClick:PropTypes.func.isRequired,
-    posts:PropTypes.arrayOf(PropTypes.shape({
-        id:PropTypes.number,
-        title:PropTypes.string,
-        text:PropTypes.string,
-    })).isRequired,
-}
+  currentPost: PropTypes.number.isRequired,
+  OnClick: PropTypes.func.isRequired,
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      title: PropTypes.string,
+      text: PropTypes.string
+    })
+  )
+};
 
 export default List;

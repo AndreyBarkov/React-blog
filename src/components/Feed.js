bd470cb965e84c89e356fe096929b88b043f31cc
Andrey Barkov <a.barkov@avanade.com>
2018-03-28 22:31:36 -0400

    New feed component

---
+++ src/components/Feed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Post from './Post';
+
+const Feed = (props) =>{
+   const feed = props.posts.map(item =><Post key={item.id} post={item} />); 
+   console.log(feed);
+   return(
+       <div>
+           {feed}
+       </div>
+   );
+}
+export default Feed;
